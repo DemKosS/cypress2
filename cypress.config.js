7d8f1698d3b9c33d1ca82a5ad2a5246ce0b2c9c8
DemKosS <sidyakinkn@gmail.com>
2022-10-26 21:41:03 +0300

    init

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: '4iicv7',
+  
+  "retries" : 1,
+  "pageLoadTimeout": 60000,     
+  "screenshotOnRunFailure" : true ,
+  e2e: {
+    baseUrl: 'http://qamid.tmweb.ru/client/index.php',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
